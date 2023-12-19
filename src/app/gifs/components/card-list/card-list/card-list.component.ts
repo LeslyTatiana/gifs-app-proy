939ebec29a9d57b6a9dbc3f393169ff7c87490c2
Lesly Zena <lesly.26igs@gmail.com>
2023-12-18 23:55:55 -0500

    first commit

---
+++ src/app/gifs/components/card-list/card-list/card-list.component.ts
@@ -0,0 +1,11 @@
+import { Gif } from 'src/app/gifs/interfaces/gifs.interfaces';
+import { Component,Input } from '@angular/core';
+
+@Component({
+  selector: 'gifs-card-list',
+  templateUrl: './card-list.component.html'
+})
+export class CardListComponent {
+  @Input()
+  public gifs: Gif[] = [];
+}
